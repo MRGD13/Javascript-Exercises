e8a27ad1a39fb7f5a0a448bcfe1b08f726300d55
skatsensei <32799750+skatsensei@users.noreply.github.com>
2021-03-27 01:02:15 -0400

    Final commit

--- guess_the_number_game/script.js
+++ guess_the_number_game/script.js
@@ -34,8 +34,7 @@ document.querySelector(".check").addEventListener("click", function () {
     //When guess is wrong
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent =
-        guess > secretNumber ? "Too High  😞" : "Too Low 😔";
+      displayMessage(guess > secretNumber ? "Too High  😞" : "Too Low 😔");
     }
     score--;
     document.querySelector(".score").textContent = score;
